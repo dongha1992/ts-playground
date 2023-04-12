f0f66d985cfc2d892bcc461604c2835b9e48f2dd
dongha1992 <kamdunga2002@gmail.com>
2023-04-12 20:30:42 +0900

    feature: UI 작업 완료

--- src/pages/InquiryCompletePage.tsx
+++ src/pages/InquiryCompletePage.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { FixedBottomCTA, Icon, List, ListRow, Lottie, Spacing } from 'components';
+import { Top22 } from 'components/Top';
+import { colors } from 'constants/colors';
 
 function InquiryCompletePage() {
-  return <div>InquiryCompletePage</div>;
+  return (
+    <>
+      <Lottie
+        src="https://static.toss.im/lotties/confetti/confetti-explode.json"
+        loop
+        css={{ position: 'fixed', width: '100%' }}
+      />
+      <Spacing size={20} />
+      <Top22 color={colors.grey900}>대출 조회 결과가 나왔어요!</Top22>
+      <Spacing size={16} />
+      <List>
+        <ListRow
+          contents={
+            <ListRow.Text2Rows
+              top="임지훈님의 맞춤 금리"
+              topProps={{ color: colors.grey900, typography: 't7' }}
+              bottom={formatRate1000(10000)}
+              bottomProps={{ color: colors.blue500, typography: 't2', fontWeight: 'semibold' }}
+            />
+          }
+        />
+        <ListRow
+          contents={
+            <ListRow.Text2Rows
+              top="한도"
+              topProps={{ color: colors.grey900, typography: 't7' }}
+              bottom={'0.3'}
+              bottomProps={{ color: colors.blue500, typography: 't2', fontWeight: 'semibold' }}
+            />
+          }
+        />
+      </List>
+      <FixedBottomCTA
+        onClick={() => {
+          // 개발 편의를 위해 앱 재시작
+          location.replace('/start');
+        }}
+      >
+        지금 바로 받으러 가기
+      </FixedBottomCTA>
+    </>
+  );
 }
 
 export default InquiryCompletePage;
+
+export function formatRate1000(rate1000: number): string {
+  return `${[(rate1000 / 10).toFixed(0), rate1000 % 10].join('.')}%`;
+}
--- src/pages/WaitingPage.tsx
+++ src/pages/WaitingPage.tsx
@@ -7,9 +7,15 @@ import { colors } from '../constants/colors';
 
 function WaitingPage() {
   const router = useCustomRouter();
+
+  useEffect(() => {
+    const handle = setInterval(() => router.push('/inquiry-complete'), 1000);
+    return () => clearTimeout(handle);
+  }, []);
   return (
     <>
       <Top22 color={colors.grey900}>{`입력한 정보로\n임지훈님의 대출 조건을 확인할게요`}</Top22>
+      <Lottie src="https://static.toss.im/lotties/loading/profile-loading.json" loop />
     </>
   );
 }
